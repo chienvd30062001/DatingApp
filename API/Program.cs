4b6663ec2e980512dee399619c9984a4854137db
Vu Duc Chien <chienndsl123456@gmail.com>
2023-05-24 10:43:37 +0700

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,31 @@
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DataContext>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddControllers();
 
 builder.Services.AddCors(p => p.AddPolicy("Cors", build =>
 {
     build.WithOrigins("https://localhost:4200").AllowAnyMethod().AllowAnyHeader();
 }));
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddIdentityServices(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -31,6 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("Cors");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
